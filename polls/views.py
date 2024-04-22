2bfca43ff221deabcd2e5f980bbb8465e72da280
Salsabil007 <salsabilarabishusmi007@gmail.com>
2024-04-21 23:22:48 -0500

    templates

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,23 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello. You are at the poll.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    #output = ", ".join([q.question_text for q in latest_question_list])
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at the results of question %s." % question_id)
+
+def results(request, question_id):
+    return HttpResponse("You are looking at the results of question %s." % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
 
