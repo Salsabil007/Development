8b3fcdebf3002cffc656b47044c5279eb6d2aca1
Salsabil007 <salsabilarabishusmi007@gmail.com>
2024-04-26 00:16:23 -0500

    user input2

--- pages/views.py
+++ pages/views.py
@@ -17,14 +17,19 @@ from django.http import HttpResponse
 def input_view(request):
     if request.method == 'POST':
         # Get the input value from the form
-        input_string = request.POST.get('input_string', '')
+        input_string = request.POST.get('input_string',[])
+        input_string2 = request.POST.get('input_string2',[])
         
         # Process the input_string (e.g., perform some operation on it)
         # For demonstration purposes, let's just return the reversed string
         reversed_string = input_string[::-1]
+        reversed_string2 = input_string2[::-1]
+        context = {}
+        context.update({'reversed_string': reversed_string})
+        context.update({'reversed_string2': reversed_string2})
         
         # Render the template with the reversed_string
-        return render(request, 'result.html', {'reversed_string': reversed_string})
+        return render(request, 'result.html', context)
     
     # If the request method is GET, render the template with an empty string
     return render(request, 'input_form.html')
